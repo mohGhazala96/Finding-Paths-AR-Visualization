8ad79347dae89b40f7c74b54d4571bb4445da373
mohGhazala96 <moh.abou.ghazala@gmail.com>
2020-01-27 14:49:59 +0300

    removed cost visulisation

--- Assets/Scripts/SearchVisualizer.cs
+++ Assets/Scripts/SearchVisualizer.cs
@@ -25,7 +25,7 @@ public class SearchVisualizer : MonoBehaviour
         yield return new WaitForSecondsRealtime(delayStep);
         GridMaker gridMaker = pathFinder.gridMaker;
         GameObject gridCell = gridMaker.gridCells[cell.x, cell.y];
-        UpdateVisual(gridCell, cell);
+        //UpdateVisual(gridCell, cell);
         gridCell.GetComponent<NodeVisual>().SetColorRed();
     }
 
@@ -35,7 +35,7 @@ public class SearchVisualizer : MonoBehaviour
 
         GridMaker gridMaker = pathFinder.gridMaker;
         GameObject gridCell = gridMaker.gridCells[cell.x, cell.y];
-        UpdateVisual(gridCell, cell);
+        //UpdateVisual(gridCell, cell);
         gridCell.GetComponent<NodeVisual>().SetColorBlue();
     }
 
@@ -45,7 +45,7 @@ public class SearchVisualizer : MonoBehaviour
 
         GridMaker gridMaker = pathFinder.gridMaker;
         GameObject gridCell = gridMaker.gridCells[cell.x, cell.y];
-        UpdateVisual(gridCell, cell);
+        //UpdateVisual(gridCell, cell);
         gridCell.GetComponent<NodeVisual>().SetColorGreen();
     }
    
